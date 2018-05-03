e3ae7c19dbf918bb0672f1e75754e4f5dea3870f
Brendan Nagle <bf.nagle@gmail.com>
2018-05-02 22:04:09 -0700

    Switching to the rain function because it's pleasant

--- weatherPuller.js
+++ weatherPuller.js
@@ -2,7 +2,7 @@ var axios = require('axios');
 const lightDriver = require('./weatherLightPatterns.js');
 var site = "http://api.wunderground.com/api/cc9f69992ffae3d7/geolookup/conditions/q/CA/San_Francisco.json";
 
-setInterval(() => lightDriver.coldest(20), 100);
+setInterval(() => lightDriver.rain(), 100);
 
 
 var handleIncomingWeatherData = function(response)
